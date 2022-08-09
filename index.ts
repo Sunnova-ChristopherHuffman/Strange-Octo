dcee6c33fe9ff3a1f9077d52507f82907ad8e1b9
ThaFishDance <chuffman91@gmail.com>
2022-08-09 18:17:51 -0500

    setting stage for javascript node and ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { isEmpty } from './isEmpty'
+
+const x = "";
+
+console.log(isEmpty(x))
---
+++ isEmpty.ts
@@ -0,0 +1,16 @@
+export const isEmpty = (e): boolean => {
+  switch (e) {
+    case "":
+    case 0:
+    case "0":
+    case null:
+    case false:
+    case undefined:
+      return true;
+    default:
+      return false;
+  }
+}
+
+
+
